2d22ac5042a12cdec3b63e5c7effa88472d1f9e6
tranquockhanhdev <tranquockhanh9a9@gmail.com>
2025-06-06 11:24:38 +0700

    first commit

---
+++ resources/js/Pages/Home.jsx
@@ -0,0 +1,7 @@
+export default function Home() {
+    return (
+        <div className="flex items-center justify-center h-screen bg-gray-100">
+            <h1 className="text-4xl font-bold text-blue-600">Welcome to the Home Page</h1>
+        </div>
+    );
+}
